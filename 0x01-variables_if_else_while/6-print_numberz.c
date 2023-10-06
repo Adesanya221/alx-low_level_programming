8c7fa94c5c2b86f3b660f1659f2d13875e243e5b
Adesanya221 <adesanyaoluwafisayomi4@gmail.com>
2023-10-06 14:55:47 -0700

    0x01. C - Variables, if, else, while

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - print numbers
+ * Return: Always 0
+ */
+int main(void)
+{
+	int d;
+
+	for (d = 48; d < 58; d++)
+		putchar(d);
+
+	putchar('\n');
+	return (0);
+}
