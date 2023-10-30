df0bb1b566fa8b0604378677134fb82638e92e42
Adesanya221 <adesanyaoluwafisayomi4@gmail.com>
2023-10-30 07:34:05 -0700

    fish

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,13 +1,60 @@
-#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
 
 /**
- * add - Adds two integers
- * @a: The first integer
- * @b: The second integer
+ * check_num - Checks if a string consists of digits
+ * @str: The input string
  *
- * Return: The sum of a and b
+ * Return: 1 if the string contains only digits, 0 otherwise
  */
-int add(int a, int b)
+int check_num(char *str)
 {
-	return (a + b);
+	unsigned int count;
+
+	count = 0;
+	while (count < strlen(str))
+	{
+		if (!isdigit(str[count]))
+		{
+			return (0);
+		}
+		count++;
+	}
+	return (1);
+}
+
+/**
+ * main - Prints the name of the program
+ * @argc: The number of arguments
+ * @argv: The array of arguments
+ *
+ * Return: Always 0 (Success)
+ */
+int main(int argc, char *argv[])
+{
+	int count;
+	int str_to_int;
+	int sum = 0;
+
+	count = 1;
+	while (count < argc)
+	{
+		if (check_num(argv[count]))
+		{
+			str_to_int = atoi(argv[count]); /* ATOI --> convert string to int */
+			sum += str_to_int;
+		}
+		else
+		{
+			printf("Error\n");
+			return (1);
+		}
+		count++;
+	}
+
+	printf("%d\n", sum); /* Print sum */
+
+	return (0);
 }
