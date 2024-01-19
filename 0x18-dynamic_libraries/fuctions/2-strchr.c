e8d774843f27246d57392a1e8e36524d450e6e26
Adesanya221 <adesanyaoluwafisayomi4@gmail.com>
2024-01-19 08:47:21 -0800

    fjf

---
+++ 0x18-dynamic_libraries/fuctions/2-strchr.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _strchr - find the first occurrence of a character in a string
+ * @s: the string to search
+ * @c: the character to find
+ *
+ * Return: a pointer to the first occurrence of character c in string s,
+ *         or NULL if the character is not found
+ */
+char *_strchr(char *s, char c)
+{
+	int i = 0;
+
+	for (; s[i] != '\0'; i++)
+	{
+		if (s[i] == c)
+			return (&s[i]);
+	}
+	return (0);
+}
